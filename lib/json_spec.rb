366bfdbfe826b4803b6f730ee0e76097f18f1959
Tikhon Botchkarev <TikiTDO@gmail.com>
2013-03-25 12:17:18 -0400

    Layout notes

--- lib/json_spec.rb
+++ lib/json_spec.rb
@@ -5,7 +5,10 @@ require_relative 'sereth_utils/all'
 require_relative 'json_spec/json_spec_utils'
 require_relative 'json_spec/json_spec_generator'
 require_relative 'json_spec/json_spec_cache'
+require_relative 'json_spec/json_spec_exports'
+require_relative 'json_spec/json_spec_imports'
 require_relative 'json_spec/json_spec_data'
 require_relative 'json_spec/json_spec_module'
 
-Binding.run_stage(:sereth_util_loaded)
+# TODO - Run any necessary stages
+# Binding.run_stage(:sereth_util_loaded)
